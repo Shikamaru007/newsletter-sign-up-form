f83d31a60681b92e0b53e9509cc8313a3773afb5
froZin <bolutolu12@gmail.com>
2024-10-22 10:45:04 +0100

    all done and made responsive

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+const form = document.getElementById("contentForm");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const emailInput = document.getElementById("email");
+    const emailPattern = /^[\w\.\-_]+@[\w\.\-_]+\.[a-z]{2,5}$/;
+
+    let isEmailValid = emailPattern.test(emailInput.value) ? true : false;
+    
+    if(!isEmailValid || emailInput === ""){
+        form.classList.add("error");
+    }else {
+        form.classList.remove("error");
+        displaySuccessTab(emailInput.value);
+    }
+
+})
+
+function displaySuccessTab(userEmail) {
+    const mainBody = document.getElementById("container");
+    const registeredEmail = document.getElementById("registeredEmail");
+
+    mainBody.classList.add("success");
+    registeredEmail.textContent = userEmail;
+
+    document.getElementById("resetBtn").addEventListener("click", ()=> {
+        resetPage(mainBody);
+    });
+}
+
+function resetPage(container) {
+    container.classList.remove("success");
+    form.reset();    
+}
+
 
